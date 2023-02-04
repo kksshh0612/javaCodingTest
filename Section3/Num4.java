c35402ca3367eb52cdb7882b9def3d29fea857a3
Kim-Seong-Ho <kksshh0612@gmail.com>
2023-02-04 17:20:43 +0900

    [secrioon3] 4번 풀이 수정

--- Section3/Num4.java
+++ Section3/Num4.java
@@ -9,20 +9,44 @@ import java.util.Scanner;
 public class Num4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> ansList = new ArrayList<>();
+
         int N1 =  scanner.nextInt();
         for(int i = 0; i < N1; i++){
-            list.add(scanner.nextInt());
+            list1.add(scanner.nextInt());
         }
         int N2 =  scanner.nextInt();
         for(int i = 0; i < N2; i++){
-            list.add(scanner.nextInt());
-        }
-        Collections.sort(list);
-        for(int i = 0; i < N1 + N2; i ++){
-            System.out.print(list.get(i) + " ");
+            list2.add(scanner.nextInt());
         }
 
-
+        int indexFisrt = 0, indexSecond = 0;
+        while(indexFisrt < N1 && indexSecond < N2){     //둘 중 하나라도 리스트 끝에 다다르면 탈출
+            if(list1.get(indexFisrt) < list2.get(indexSecond)){
+                ansList.add(list1.get(indexFisrt));
+                indexFisrt++;
+            }
+            else{
+                ansList.add(list2.get(indexSecond));
+                indexSecond++;
+            }
+        }
+//        if(indexFisrt < N1){
+//            ansList.addAll(indexFisrt, list1);
+//        }
+//        if(indexSecond < N2){
+//            ansList.addAll(indexSecond, list2);
+//        }
+        while(indexFisrt < N1){
+            ansList.add(list1.get(indexFisrt++));
+        }
+        while(indexSecond < N2){
+            ansList.add(list2.get(indexSecond++));
+        }
+        for(int i = 0; i < ansList.size(); i++){
+            System.out.print(ansList.get(i) + " ");
+        }
     }
 }
