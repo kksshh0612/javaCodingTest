13a1de040a9dee11baa1cbfd088387723adf561d
Kim-Seong-Ho <kksshh0612@gmail.com>
2023-02-06 16:54:58 +0900

    [section3] 5번 알고리즘 개선

--- Section3/Num5.java
+++ Section3/Num5.java
@@ -2,7 +2,7 @@ package Section3;
 
 import java.util.Scanner;
 
-//5. 수들의 합
+//5. 수들의 합(알고리즘 개선 후
 public class Num5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -14,20 +14,58 @@ public class Num5 {
         }
         int indexL = 0;
         int indexR = 1;
-        int sum = 0, cnt = 0;
+        int sum = arr[indexL], cnt = 0;
 
-        while(indexL < n){      //왼쪽이 마지막에 다다를 때까지 반복
-            sum = arr[indexL];
-            indexR = indexL + 1;
-
-            while(sum < m && indexR < n){
-                sum += arr[indexR++];
+        while(true){
+            if(sum < m){        //합이 작으면
+                if(indexR < n){
+                    sum += arr[indexR++];
+                }
+                else{
+                    break;
+                }
             }
-            if(sum == m){
+            else if(sum == m){
                 cnt++;
+                sum -= arr[indexL];
+                indexL++;
             }
-            indexL++;
+            else{               //합이 더 크면
+                sum -= arr[indexL];
+                indexL++;
+            }
         }
         System.out.println(cnt);
     }
 }
+
+
+////5. 수들의 합 (알고리즘 개선 전)
+//public class Num5 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
+//        int m = scanner.nextInt();
+//        int[] arr = new int[n];
+//        for(int i = 0; i < n; i++){
+//            arr[i] = scanner.nextInt();
+//        }
+//        int indexL = 0;
+//        int indexR = 1;
+//        int sum = 0, cnt = 0;
+//
+//        while(indexL < n){      //왼쪽이 마지막에 다다를 때까지 반복
+//            sum = arr[indexL];
+//            indexR = indexL + 1;
+//
+//            while(sum < m && indexR < n){
+//                sum += arr[indexR++];
+//            }
+//            if(sum == m){
+//                cnt++;
+//            }
+//            indexL++;
+//        }
+//        System.out.println(cnt);
+//    }
+//}
